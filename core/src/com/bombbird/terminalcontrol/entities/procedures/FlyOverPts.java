b7cfa1562e67c58e3c9fac1fca2f15f2e164eb10
Shang Yi Ku <kushangyi2001@gmail.com>
2019-01-06 21:52:19 +0800

    I'm just dumb

--- core/src/com/bombbird/terminalcontrol/entities/procedures/FlyOverPts.java
+++ core/src/com/bombbird/terminalcontrol/entities/procedures/FlyOverPts.java
@@ -8,7 +8,7 @@ public class FlyOverPts {
 
     //Loads the fly over points
     public static void loadPoints() {
-        FLY_OVER_PTS.put("RCTP", new String[] {"TP050", "TP060", "TP064", "TP230", "TP240", "MOLKA", "OCTAN"});
+        FLY_OVER_PTS.put("RCTP", new String[] {"TP050", "TP060", "TP064", "TP230", "TP240", "OCTAN", "NEPAS"});
         FLY_OVER_PTS.put("RJTT", new String[] {"TT501"});
         FLY_OVER_PTS.put("RJAA", new String[] {"ASPEN", "BEAMS", "ARIES", "BOXER", "ASTRA"});
         FLY_OVER_PTS.put("RJBB", new String[] {"B6R10", "B6R13", "B6R14", "B4L10", "B4R20", "B6L20"});
