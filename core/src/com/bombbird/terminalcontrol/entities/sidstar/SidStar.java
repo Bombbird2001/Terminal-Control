9a15823ad6666259e89a3238acba7858d750e0fe
Shang Yi Ku <kushangyi2001@gmail.com>
2019-04-22 20:50:49 +0800

    Stuff

--- core/src/com/bombbird/terminalcontrol/entities/sidstar/SidStar.java
+++ core/src/com/bombbird/terminalcontrol/entities/sidstar/SidStar.java
@@ -49,7 +49,7 @@ public class SidStar {
     }
 
     private void drawOutbound(float previousX, float previousY, int outbound) {
-        if (outbound != -1) {
+        if (outbound != -1 && previousX <= 4500 && previousX >= 1260 && previousY <= 3240 && previousY >= 0) {
             float outboundTrack = outbound - radarScreen.magHdgDev;
             float[] point = MathTools.pointsAtBorder(new float[] {1260, 4500}, new float[] {0, 3240}, previousX, previousY, outboundTrack);
             radarScreen.shapeRenderer.line(previousX, previousY, point[0], point[1]);
