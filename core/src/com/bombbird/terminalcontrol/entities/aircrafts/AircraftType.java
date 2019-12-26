c3d673f91a074be1f321d0c441c0097e6e91bf07
Shang Yi Ku <kushangyi2001@gmail.com>
2019-12-26 19:52:24 +0800

    Update some aircraft data, add callsigns (for LFPG), update gradle build tools

--- core/src/com/bombbird/terminalcontrol/entities/aircrafts/AircraftType.java
+++ core/src/com/bombbird/terminalcontrol/entities/aircrafts/AircraftType.java
@@ -8,26 +8,32 @@ public class AircraftType {
     private static final HashMap<String, int[]> aircraftTypes = FileLoader.loadAircraftData();
 
     public static char getWakeCat(String type) {
+        if (!aircraftTypes.containsKey(type)) return 'M';
         return (char) aircraftTypes.get(type)[0];
     }
 
     public static char getRecat(String type) {
+        if (!aircraftTypes.containsKey(type)) return 'D';
         return (char) aircraftTypes.get(type)[5];
     }
 
     public static int getV2(String type) {
+        if (!aircraftTypes.containsKey(type)) return 150;
         return aircraftTypes.get(type)[1];
     }
 
     public static int getTypClimb(String type) {
+        if (!aircraftTypes.containsKey(type)) return 2000;
         return aircraftTypes.get(type)[2];
     }
 
     public static int getTypDes(String type) {
+        if (!aircraftTypes.containsKey(type)) return 2000;
         return aircraftTypes.get(type)[3];
     }
 
     public static int getApchSpd(String type) {
+        if (!aircraftTypes.containsKey(type)) return 150;
         return aircraftTypes.get(type)[4];
     }
 }
