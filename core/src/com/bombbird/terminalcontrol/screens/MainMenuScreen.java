ba829a921c06c0a6b2d2114a4a098634fd841dee
Shang Yi Ku <kushangyi2001@gmail.com>
2020-02-04 20:25:47 +0800

    Change glclear to include more clear masks, hopefully fix the flickering issue

--- core/src/com/bombbird/terminalcontrol/screens/MainMenuScreen.java
+++ core/src/com/bombbird/terminalcontrol/screens/MainMenuScreen.java
@@ -212,7 +212,7 @@ public class MainMenuScreen implements Screen {
     @Override
     public void render(float delta) {
         Gdx.gl.glClearColor(0, 0, 0, 0);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT | (Gdx.graphics.getBufferFormat().coverageSampling?GL20.GL_COVERAGE_BUFFER_BIT_NV:0));
 
         camera.update();
 
--- core/src/com/bombbird/terminalcontrol/screens/NotifScreen.java
+++ core/src/com/bombbird/terminalcontrol/screens/NotifScreen.java
@@ -112,7 +112,7 @@ public class NotifScreen implements Screen {
         timer.scheduleTask(new Timer.Task() {
             @Override
             public void run() {
-                Gdx.app.postRunnable(() -> {backButton.setVisible(true);});
+                Gdx.app.postRunnable(() -> backButton.setVisible(true));
             }
         }, 5f);
     }
@@ -127,7 +127,7 @@ public class NotifScreen implements Screen {
     @Override
     public void render(float delta) {
         Gdx.gl.glClearColor(0, 0, 0, 0);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT | (Gdx.graphics.getBufferFormat().coverageSampling?GL20.GL_COVERAGE_BUFFER_BIT_NV:0));
 
         camera.update();
 
