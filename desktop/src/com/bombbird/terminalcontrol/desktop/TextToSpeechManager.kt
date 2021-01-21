782e93881a1e9daaacc2b70632712c4535bf63e3
Shang Yi Ku <kushangyi2001@gmail.com>
2021-01-21 22:26:48 +0800

    Add more MaryTTS stuff, but for some reason the imported voices are not loading on startup

--- desktop/src/com/bombbird/terminalcontrol/desktop/TextToSpeechManager.kt
+++ desktop/src/com/bombbird/terminalcontrol/desktop/TextToSpeechManager.kt
@@ -4,24 +4,55 @@ import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.utils.Array
 import com.bombbird.terminalcontrol.sounds.TextToSpeechInterface
 import com.bombbird.terminalcontrol.utilities.files.FileLoader
+import marytts.LocalMaryInterface
+import marytts.modules.synthesis.Voice
+import marytts.util.data.audio.AppendableSequenceAudioInputStream
+import marytts.util.data.audio.AudioPlayer
+import org.apache.commons.lang3.SystemUtils
 import java.io.BufferedReader
 import java.io.InputStreamReader
+import java.lang.Exception
 
 class TextToSpeechManager : TextToSpeechInterface {
+    private var supportsSAPI = false
     private val voices = Array<String>()
+    private var maryInterface: LocalMaryInterface? = null
+    private var audioPlayer: AudioPlayer? = null
+    private var maryAudioSequence: AppendableSequenceAudioInputStream? = null
 
     /** Says the text */
     override fun sayText(text: String, voice: String) {
-        Thread {
-            Runtime.getRuntime().exec("${Gdx.files.external(FileLoader.mainDir + "/tts/balcon.exe").file().absolutePath} -s 2 -t \"$text\"${if (voices.contains(voice)) " -n \"$voice\"" else ""}")
-        }.start()
+        if (supportsSAPI) {
+            Thread {
+                Runtime.getRuntime().exec(
+                    "${
+                        Gdx.files.external(FileLoader.mainDir + "/tts/balcon.exe").file().absolutePath
+                    } -s 2 -t \"$text\"${if (voices.contains(voice)) " -n \"$voice\"" else ""}"
+                )
+            }.start()
+        } else {
+            try {
+                maryInterface?.voice = voice
+                val audio = maryInterface?.generateAudio(text)
+                if (audio != null) {
+                    maryAudioSequence?.append(audio)
+                    if (audioPlayer?.isAlive == false) audioPlayer?.start()
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
     }
 
     /** Stops all current and subsequent speeches */
     override fun cancel() {
-        Thread {
-            Runtime.getRuntime().exec("${Gdx.files.external(FileLoader.mainDir + "/tts/balcon.exe").file().absolutePath} -k")
-        }.start()
+        if (supportsSAPI) {
+            Thread {
+                Runtime.getRuntime().exec("${Gdx.files.external(FileLoader.mainDir + "/tts/balcon.exe").file().absolutePath} -k")
+            }.start()
+        } else {
+            audioPlayer?.cancel()
+        }
     }
 
     /** Checks if the voice is available, returns original voice if it is, else returns a random voice from all available voices */
@@ -34,34 +65,46 @@ class TextToSpeechManager : TextToSpeechInterface {
     override fun loadVoices() {
         if (!voices.isEmpty) return
 
-        val enVoices = HashSet<String>()
-        enVoices.add("James")
-        enVoices.add("Catherine")
-        enVoices.add("Richard")
-        enVoices.add("Linda")
-        enVoices.add("George")
-        enVoices.add("Hazel")
-        enVoices.add("Susan")
-        enVoices.add("Ravi")
-        enVoices.add("Heera")
-        enVoices.add("Shaun")
-        enVoices.add("David")
-        enVoices.add("Mark")
-        enVoices.add("Zira")
+        if (SystemUtils.IS_OS_WINDOWS_10 || SystemUtils.IS_OS_WINDOWS_8 || SystemUtils.IS_OS_WINDOWS_7 || SystemUtils.IS_OS_WINDOWS_VISTA || SystemUtils.IS_OS_WINDOWS_XP) {
+            val enVoices = Array<String>()
+            enVoices.add("James", "Catherine", "Richard", "Linda")
+            enVoices.add("George", "Hazel", "Susan", "Ravi")
+            enVoices.add("Heera", "Shaun", "David", "Mark")
+            enVoices.add("Zira")
+            Thread {
+                val version = Gdx.files.internal("tts/version.txt")
+                val currentVersion = Gdx.files.external(FileLoader.mainDir + "/tts/version.txt")
+                if (!currentVersion.exists() || version.readString() != currentVersion.readString()) Gdx.files.internal("tts").copyTo(Gdx.files.external(FileLoader.mainDir))
 
-        Thread {
-            val version = Gdx.files.internal("tts/version.txt")
-            val currentVersion = Gdx.files.external(FileLoader.mainDir + "/tts/version.txt")
-            if (!currentVersion.exists() || version.readString() != currentVersion.readString()) Gdx.files.internal("tts").copyTo(Gdx.files.external(FileLoader.mainDir))
+                val process = Runtime.getRuntime().exec("${Gdx.files.external(FileLoader.mainDir + "/tts/balcon.exe").file().absolutePath} -l")
+                val reader = BufferedReader(InputStreamReader(process.inputStream))
+                process.waitFor()
+                reader.forEachLine {
+                    val name = it.trim()
+                    if (name.isNotBlank() && !name.contains("SAPI") && enVoices.contains(name.split(" ")[1])) voices.add(name)
+                }
+                println(voices)
+                supportsSAPI = true
+                if (voices.isEmpty) {
+                    //If user does not have any available voices installed, use MaryTTS
+                    loadMary()
+                }
+            }.start()
+        } else {
+            //If user OS does not support Microsoft SAPI, use MaryTTS
+            loadMary()
+        }
+    }
 
-            val process = Runtime.getRuntime().exec("${Gdx.files.external(FileLoader.mainDir + "/tts/balcon.exe").file().absolutePath} -l")
-            val reader = BufferedReader(InputStreamReader(process.inputStream))
-            process.waitFor()
-            reader.forEachLine {
-                val name = it.trim()
-                if (name.isNotBlank() && !name.contains("SAPI") && enVoices.contains(name.split(" ")[1])) voices.add(name)
-            }
-            println(voices)
-        }.start()
+    private fun loadMary() {
+        supportsSAPI = false
+        for (voice in Voice.getAvailableVoices()) {
+            voices.add(voice.name)
+        }
+        println(voices)
+        if (voices.isEmpty) return
+        maryInterface = LocalMaryInterface()
+        maryAudioSequence = AppendableSequenceAudioInputStream(Voice.AF22050, null)
+        audioPlayer = AudioPlayer(maryAudioSequence)
     }
 }
