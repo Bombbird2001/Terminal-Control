b7c4a36f4bdcba7f1565c6c826497214948f44d4
Shang Yi Ku <kushangyi2001@gmail.com>
2021-02-05 21:01:42 +0800

    Some work on drive saving stuff

--- android/src/com/bombbird/terminalcontrol/DriveManager.kt
+++ android/src/com/bombbird/terminalcontrol/DriveManager.kt
@@ -1,5 +1,6 @@
 package com.bombbird.terminalcontrol
 
+import com.bombbird.terminalcontrol.utilities.files.FileLoader
 import com.google.api.services.drive.Drive
 import com.google.api.client.http.FileContent
 import com.google.api.services.drive.model.File
@@ -8,31 +9,29 @@ import com.google.api.services.drive.model.FileList
 
 class DriveManager(private val drive: Drive) {
     fun createFile() {
-        println("Create file stub")
-        /*
         val fileMetadata = File()
         fileMetadata.name = "settings.json"
         fileMetadata.parents = Collections.singletonList("appDataFolder")
-        val filePath = java.io.File("files/config.json")
+        val filePath = FileLoader.getExtDir("settings.json")?.file()
         val mediaContent = FileContent("application/json", filePath)
-        val file: File = drive.files().create(fileMetadata, mediaContent)
-            .setFields("id")
-            .execute()
-        println("Created file ID: " + file.id)
-         */
+        Thread {
+            val file: File = drive.files().create(fileMetadata, mediaContent)
+                .setFields("id")
+                .execute()
+            println("Created file ID: " + file.id)
+        }.start()
     }
 
     fun listFiles() {
-        println("List files stub")
-        /*
-        val files: FileList = drive.files().list()
-            .setSpaces("appDataFolder")
-            .setFields("nextPageToken, files(id, name)")
-            .setPageSize(10)
-            .execute()
-        for (file in files.files) {
-            println("Found file: ${file.name}, ${file.id}")
-        }
-         */
+        println("List files")
+        Thread {
+            val files: FileList = drive.files().list()
+                .setSpaces("appDataFolder")
+                .execute()
+            for (file in files.files) {
+                println("Found file: ${file.name}, ${file.id}")
+                if (file.name == "config.json") drive.files().delete(file.id)
+            }
+        }.start()
     }
 }
