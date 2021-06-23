2d9ae36fd855c8988d5946e03dd41913ea681eee
Shang Yi Ku <kushangyi2001@gmail.com>
2021-06-23 22:17:06 +0800

    A bit more work

--- core/src/com/bombbird/terminalcontrol/ui/datatag/DataTagConfig.kt
+++ core/src/com/bombbird/terminalcontrol/ui/datatag/DataTagConfig.kt
@@ -1,16 +1,16 @@
 package com.bombbird.terminalcontrol.ui.datatag
 
 import com.badlogic.gdx.utils.Array
+import com.bombbird.terminalcontrol.utilities.files.FileLoader
 import org.json.JSONObject
 
-class DataTagConfig {
+class DataTagConfig(var name: String) {
     companion object {
         const val DEFAULT = "Default"
         const val COMPACT = "Compact"
         val CAN_BE_HIDDEN = HashSet<String>(listOf(DataTag.CLEARED_ALT, DataTag.LAT_CLEARED, DataTag.SIDSTAR_CLEARED, DataTag.CLEARED_IAS))
     }
 
-    lateinit var name: String
     var onlyShowWhenChanged: HashSet<String> = HashSet()
     var arrangement: Array<Array<String>> = Array()
     var miniArrangement = Pair<Array<Array<String>>, Array<Array<String>>>(Array(3), Array(3))
@@ -21,10 +21,39 @@ class DataTagConfig {
         arrangement.add(Array(4), Array(4), Array(4), Array(4))
         miniArrangement.first.add(Array(2), Array(2), Array(2))
         miniArrangement.second.add(Array(2), Array(2), Array(2))
+
+        when (name) {
+            DEFAULT -> {
+                arrangement[0].add(DataTag.CALLSIGN, DataTag.ICAO_TYPE_WAKE)
+                arrangement[1].add(DataTag.ALTITUDE_FULL)
+                arrangement[2].add(DataTag.HEADING, DataTag.LAT_CLEARED, DataTag.SIDSTAR_CLEARED)
+                arrangement[3].add(DataTag.GROUND_SPEED, DataTag.CLEARED_IAS)
+                miniArrangement.first[0].add(DataTag.CALLSIGN_RECAT)
+                miniArrangement.first[1].add(DataTag.ALTITUDE, DataTag.HEADING)
+                miniArrangement.first[2].add(DataTag.GROUND_SPEED)
+                firstEmpty = false
+            }
+            COMPACT -> {
+                arrangement[0].add(DataTag.CALLSIGN, DataTag.ICAO_TYPE_WAKE)
+                arrangement[1].add(DataTag.ALTITUDE_FULL)
+                arrangement[2].add(DataTag.LAT_CLEARED, DataTag.SIDSTAR_CLEARED)
+                arrangement[3].add(DataTag.GROUND_SPEED, DataTag.CLEARED_IAS)
+                miniArrangement.first[0].add(DataTag.CALLSIGN_RECAT)
+                miniArrangement.first[1].add(DataTag.ALTITUDE, DataTag.GROUND_SPEED)
+                miniArrangement.second[0].add(DataTag.CALLSIGN_RECAT)
+                miniArrangement.second[1].add(DataTag.CLEARED_ALT, DataTag.ICAO_TYPE)
+                onlyShowWhenChanged.add(DataTag.LAT_CLEARED)
+                firstEmpty = false
+                secondEmpty = false
+            }
+            else -> {
+                val dataObject = FileLoader.getDatatagLayout(name)
+                if (dataObject != null) loadFromSave(dataObject)
+            }
+        }
     }
 
-    constructor(option: String, jsonObject: JSONObject) {
-        name = option
+    private fun loadFromSave(jsonObject: JSONObject) {
         val showWhenChanged = jsonObject.getJSONArray("showWhenChanged")
         for (i in 0 until showWhenChanged.length()) {
             onlyShowWhenChanged.add(showWhenChanged.getString(i))
@@ -57,38 +86,6 @@ class DataTagConfig {
 
         firstEmpty = miniArrangement.first[0].isEmpty && miniArrangement.first[1].isEmpty && miniArrangement.first[2].isEmpty
         secondEmpty = miniArrangement.second[0].isEmpty && miniArrangement.second[1].isEmpty && miniArrangement.second[2].isEmpty
-    }
-
-    constructor(option: String) {
-        name = option
-        when (option) {
-            DEFAULT -> {
-                arrangement[0].add(DataTag.CALLSIGN, DataTag.ICAO_TYPE_WAKE)
-                arrangement[1].add(DataTag.ALTITUDE_FULL)
-                arrangement[2].add(DataTag.HEADING, DataTag.LAT_CLEARED, DataTag.SIDSTAR_CLEARED)
-                arrangement[3].add(DataTag.GROUND_SPEED, DataTag.CLEARED_IAS)
-                miniArrangement.first[0].add(DataTag.CALLSIGN_RECAT)
-                miniArrangement.first[1].add(DataTag.ALTITUDE, DataTag.HEADING)
-                miniArrangement.first[2].add(DataTag.GROUND_SPEED)
-                firstEmpty = false
-            }
-            COMPACT -> {
-                arrangement[0].add(DataTag.CALLSIGN, DataTag.ICAO_TYPE_WAKE)
-                arrangement[1].add(DataTag.ALTITUDE_FULL)
-                arrangement[2].add(DataTag.LAT_CLEARED, DataTag.SIDSTAR_CLEARED)
-                arrangement[3].add(DataTag.GROUND_SPEED, DataTag.CLEARED_IAS)
-                miniArrangement.first[0].add(DataTag.CALLSIGN_RECAT)
-                miniArrangement.first[1].add(DataTag.ALTITUDE, DataTag.GROUND_SPEED)
-                miniArrangement.second[0].add(DataTag.CALLSIGN_RECAT)
-                miniArrangement.second[1].add(DataTag.CLEARED_ALT, DataTag.ICAO_TYPE)
-                onlyShowWhenChanged.add(DataTag.LAT_CLEARED)
-                firstEmpty = false
-                secondEmpty = false
-            }
-            else -> {
-                //TODO Load datatag json file with config, if name exists
-            }
-        }
     }
 
     fun showOnlyWhenChanged(field: String): Boolean {
